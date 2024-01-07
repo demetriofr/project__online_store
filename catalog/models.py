a639841a7b90d92cd1d20cac657f61cfa75e4131
demetriofr <dmitriy.a.frolov@gmail.com>
2024-01-08 01:38:21 +0300

    The max_length parameter for the price_per_one field was replaced with max_digits, the created_at field was added in the Catalog class, a migration was applied, then the migration was rolled back, and therefore, task 4 was completed.

--- catalog/models.py
+++ catalog/models.py
@@ -9,7 +9,7 @@ class Product(models.Model):
     description = models.TextField(max_length=1000, verbose_name='описание')
     preview = models.ImageField(upload_to='product/', verbose_name='превью', **NULLABLE)
     category = models.CharField(max_length=100, verbose_name='категория')
-    price_per_one = models.DecimalField(max_length=10, decimal_places=2, verbose_name='цена за штуку')
+    price_per_one = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='цена за штуку')
     date_of_creation = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
     date_of_change = models.DateTimeField(auto_now=True, verbose_name='дата создания')
 
@@ -25,6 +25,7 @@ class Product(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='наименование')
     description = models.TextField(max_length=1000, verbose_name='описание')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания', **NULLABLE)
 
     def __str__(self):
         return f'{self.name}'
