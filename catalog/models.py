03ce783dd0897ef5e07d43d0b0f2a20281b7608d
demetriofr <dmitriy.a.frolov@gmail.com>
2024-01-08 00:21:38 +0300

    Added necessary additions to task 3

--- catalog/models.py
+++ catalog/models.py
@@ -13,7 +13,23 @@ class Product(models.Model):
     date_of_creation = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
     date_of_change = models.DateTimeField(auto_now=True, verbose_name='дата создания')
 
+    def __str__(self):
+        return f'{self.name} {self.category}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ('name', '-date_of_creation',)
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='наименование')
     description = models.TextField(max_length=1000, verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ('name',)
