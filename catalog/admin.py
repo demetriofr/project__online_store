7ef67a1a543aa3eb28a00600eba3c15470dece2e
demetriofr <dmitriy.a.frolov@gmail.com>
2024-01-08 04:02:19 +0300

    Implemented a display function in the admin panel. Task 5 completed.

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Product, Category
+
+# admin.site.register(Product)
+# admin.site.register(Category)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price_per_one', 'category',)
+    list_filter = ('category',)
+    search_fields = ('name__icontains', 'description__icontains',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
