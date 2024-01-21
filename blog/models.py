85f7735184ce1f9e7f2d5351ba24e05b29fbeab3
demetriofr <dmitriy.a.frolov@gmail.com>
2024-01-22 01:32:31 +0300

    Add Blog app as well as model, template, etc

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+NULLABLE = {'blank': True, 'null': True}
+# Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100, verbose_name='заголовок')
+    slug = models.CharField(max_length=100, verbose_name='slug')
+    content = models.TextField(max_length=2000, verbose_name='содержимое')
+    preview = models.ImageField(upload_to='blog/', verbose_name='превью', **NULLABLE)
+    date_create = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+    publication_indicator = models.BooleanField(default=True, verbose_name='признак публикации')
+    number_of_view = models.IntegerField(default=0, verbose_name='количество просмотров')
---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+
+from blog.models import Blog
+
+
+# Create your views here.
+
+class BlogListView(ListView):
+    model = Blog
